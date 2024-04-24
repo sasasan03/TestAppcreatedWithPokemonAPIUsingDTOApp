e118378e63f7239811811490dedf8f95091f46bb
sasasan03 <sako.hiro62@gmail.com>
2024-04-24 21:17:00 +0900

    change: ファイル更新

---
+++ TestAppcreatedWithPokemonAPIUsingDTO/URL.swift
@@ -0,0 +1,12 @@
+//
+//  URL.swift
+//  TestAppcreatedWithPokemonAPIUsingDTO
+//
+//  Created by sako0602 on 2024/04/24.
+//
+
+import Foundation
+
+class URLLink {
+    let urlString = "https://662656e5052332d55322860a.mockapi.io/api/v1/new_pokemon"
+}
