304cd813ac0460b4127b4cc18b35d4fcc3d0e33a
sasasan03 <sako.hiro62@gmail.com>
2024-04-28 13:59:53 +0900

    feat: テストコードの練習用のファイルを追加

---
+++ TestAppcreatedWithPokemonAPIUsingDTO/TestCalculator .swift
@@ -0,0 +1,19 @@
+//
+//  TestCalculator .swift
+//  TestAppcreatedWithPokemonAPIUsingDTO
+//
+//  Created by sako0602 on 2024/04/28.
+//
+
+import Foundation
+
+class Calculator {
+    func add(_ a: Int,_ b: Int) -> Int{
+        a + b
+    }
+    
+    func sub(_ a: Int,_ b: Int) -> Int{
+        a - b
+    }
+}
+
--- TestAppcreatedWithPokemonAPIUsingDTOTests/TestAppcreatedWithPokemonAPIUsingDTOTests.swift
+++ TestAppcreatedWithPokemonAPIUsingDTOTests/TestAppcreatedWithPokemonAPIUsingDTOTests.swift
@@ -8,6 +8,55 @@
 import XCTest
 @testable import TestAppcreatedWithPokemonAPIUsingDTO
 
+//class TestCalculatorTest: XCTestCase {
+//    func testAdd() {
+//        let calclatore = Calculator()
+//        
+//        let resulut = calclatore.add(10, 20)
+//        
+//        XCTAssertEqual(resulut, 30, "答えは30です")
+//    }
+//    
+//    func testSub() {
+//        let calclatore = Calculator()
+//        
+//        let resulut = calclatore.sub(30, 20)
+//        
+//        XCTAssertEqual(resulut, 10, "答えは30です")
+//    }
+//}
+
+class TestCalculatorTest: XCTestCase {
+    //　共通で使うクラスを宣言する
+    var calc: Calculator!
+    
+    // 上記で言う、let calclatore = Calculator()をここで行なっている。
+    override func setUp() {
+        super.setUp()
+        self.calc = Calculator()
+    }
+    
+    // 全てのテストのあと処理を書くことができる。
+    override func tearDown() {
+        super.tearDown()
+    }
+    
+    func testAdd(){
+        XCTAssertEqual(calc.add(10, 20), 30)
+    }
+    
+    func testSub(){
+        XCTAssertEqual(calc.sub(30, 10), 20)
+    }
+}
+
+
+
+
+
+
+
+
 final class TestAppcreatedWithPokemonAPIUsingDTOTests: XCTestCase {
 
     override func setUpWithError() throws {
