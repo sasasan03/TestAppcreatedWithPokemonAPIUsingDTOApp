f9cf53beab8fa9566ffe6466855bcf6148fa3073
sasasan03 <sako.hiro62@gmail.com>
2024-05-23 10:53:20 +0900

    feat: GitHubAPIのテストファイルを追加

---
+++ TestAppcreatedWithPokemonAPIUsingDTOTests/GitHubAPITests.swift
@@ -0,0 +1,11 @@
+//
+//  GitHubAPITests.swift
+//  TestAppcreatedWithPokemonAPIUsingDTOTests
+//
+//  Created by sako0602 on 2024/05/23.
+//
+
+import Foundation
+import XCTest
+@testable import TestAppcreatedWithPokemonAPIUsingDTO
+
